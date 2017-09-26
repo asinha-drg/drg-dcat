f8840575a67b254a93c8facdf2641bbeeb5beafa
Akshansh Sinha <asinha@teamdrg.com>
2017-09-26 14:58:56 +0530

    Code fix for processfile.php
    Query fixed and header added

--- dcat/processfile.php
+++ dcat/processfile.php
@@ -3,12 +3,14 @@
 	if((isset($_GET['projectid'])) AND (isset($_GET['fileid']))) {
 		$projectid = $_GET['projectid'];
 		$fileid = $_GET['fileid'];
-		$query = "select process_digitel_data($projectid, $fileid)";
+		$query = "select process_digital_data($projectid, $fileid)";
 		if (!pg_connection_busy($dbconn)) {
 			pg_send_query($dbconn,$query) or die('Query failed: ' . pg_last_error());
 			$result = pg_get_result($dbconn);
 			pg_query("update files set status = 'Processing' where id = $fileid and project_id = $projectid") or die('Query failed: ' . pg_last_error());
 			pg_query("commit");
+
+		header("location: runproject.php?projectid=$projectid");
 		}
 	}
 ?>
